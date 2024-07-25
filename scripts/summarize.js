6068eff4d562649a0a29e2aa159121a209dc0ae6
Peter <peteryuli1204@gmail.com>
2024-07-24 19:35:11 -0700

    Built temporary system to load api key from untracked json file

--- scripts/summarize.js
+++ scripts/summarize.js
@@ -1,9 +1,34 @@
-const allPTags = document.getElementsByTagName("p");
+// fetch('config.json')
+//     .then(response => response.json)
+//     .then(config => {
+//         const apiKey = config.apiKey;
+        
+//         const allPTags = document.getElementsByTagName("p");
 
-var articleText = "";
+//         var articleText = "";
 
-for (const pTag of allPTags) {
-    articleText += " " + pTag.textContent;
-}
+//         for (const pTag of allPTags) {
+//             articleText += " " + pTag.textContent;
+//         }
+        
+//         console.log(articleText)
 
-console.log(articleText);
+//         console.log(apiKey)
+//     })
+//     .catch(error => console.error('API Key could not be fetched', error))
+
+const configUrl = chrome.runtime.getURL('config.json');
+
+fetch(configUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Bad Network Response");
+        }
+        return response.json();
+    })
+    .then(config => {
+        console.log(config.apiKey)
+    })
+    .catch(error => {
+        console.error('Error fetching config', error)
+    })
